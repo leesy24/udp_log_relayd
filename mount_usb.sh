116ec9b5e4d74b59e76bb6dd8f35da548f405088
Sang-young Lee <leesy24@gmail.com>
2019-02-01 16:31:55 +0900

    Update to check sda1 also

--- mount_usb.sh
+++ mount_usb.sh
@@ -1,10 +1,22 @@
 #! /bin/sh
 
 sleep 1
-while [ ! -e /dev/sdb1 ]; do
+while [ ! -e /dev/sda1 ] && [ ! -e /dev/sdb1 ]; do
 	sleep 1
+	echo "retry...find /dev/sda1 or sdb1"
 done
 
-echo "mount /dev/sdb1 on /tmp/usb."
+if [ -e /tmp/mmc ]; then
+	##----------------(if used MMC from pinetd.c)##
+	umount -f /tmp/mmc
+fi
+
+echo "make /tmp/usb"
 mkdir -p /tmp/usb
-mount -t vfat /dev/sdb1 /tmp/usb
+if [ -e /dev/sda1 ]; then
+	echo "mount /dev/sda1 on /tmp/usb"
+	mount -t vfat /dev/sda1 /tmp/usb
+elif [ -e /dev/sdb1 ]; then
+	echo "mount /dev/sdb1 on /tmp/usb"
+	mount -t vfat /dev/sdb1 /tmp/usb
+fi
